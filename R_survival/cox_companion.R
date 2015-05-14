1756e28370d8ce4da8fd5cd5e49100edf29cf347
yama1968 <ymartel@gmail.com>
2015-05-14 17:33:01 +0200

    Re survival

--- R_survival/cox_companion.R
+++ R_survival/cox_companion.R
@@ -2,6 +2,7 @@
 
 
 library(survival)
+library(car)
 args(coxph)
 
 url <- "http://socserv.mcmaster.ca/jfox/Books/Companion/data/Rossi.txt"
@@ -27,5 +28,46 @@ plot(survfit(mod.allison, newdata=Rossi.fin), conf.int=TRUE,
      lty=c(1, 2), ylim=c(0.6, 1), xlab="Weeks",
      ylab="Proportion Not Rearrested")
 legend("bottomleft", legend=c("fin = no", "fin = yes"), lty=c(1 ,2), inset=0.02)
+
+
+Rossi.2 <- unfold(Rossi, time="week",
+                  event="arrest", cov=11:62, cov.names="employed")
+
+mod.allison.2 <- coxph(Surv(start, stop, arrest.time) ~
+                         fin + age + race + wexp + mar + paro + prio + employed,
+                       data=Rossi.2)
+summary(mod.allison.2)
+
+
+Rossi.3 <- unfold(Rossi, "week", "arrest", 11:62, "employed", lag=1)
+mod.allison.3 <- coxph(Surv(start, stop, arrest.time) ~
+                         fin + age + race + wexp + mar + paro + prio + employed,
+                       data=Rossi.3)
+summary(mod.allison.3)
+
+mod.allison.4 <- coxph(Surv(week, arrest) ~ fin + age + prio,
+                       data = Rossi)
+summary(mod.allison.4)
+
+czph <- cox.zph(mod.allison.4)
+czph
+par(mfrow=c(2, 2))
+plot(czph)
+par(mfrow=c(1,1))
+
+mod.allison.5 <- coxph(Surv(start, stop, arrest.time) ~
+                         fin + age + age:stop + prio,
+                       data=Rossi.2)
+mod.allison.5
+
+Rossi$age.cat <- recode(Rossi$age, " lo:19=1; 20:25=2; 26:30=3; 31:hi=4 ")
+xtabs(~ age.cat, data=Rossi)
+
+mod.allison.6 <- coxph(Surv(week, arrest) ~
+                         fin + prio + strata(age.cat), data=Rossi)
+mod.allison.6
+
+
+
 
 
