490a70c8c146ec8cba827e4076a24cf11a6cf8e3
yama1968 <ymartel@gmail.com>
2015-07-05 11:46:02 +0200

    addition of parallel 7 scientists

---
+++ pymc/7stateux_model.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+(nb_stateux, sample_length) = sample.shape
+
+v_mus = np.array([pm.Normal("mu%d" % i, mu=0, tau=0.001) for i in np.arange(sample_length)])
+v_sigmas = [pm.Uniform("sigma%d" % k, lower=0, upper=100) for k in np.arange(nb_stateux)]
+v_taus = [pm.Lambda("tau%d" % k, lambda sigma=v_sigmas[k]: 1/sigma**2) for k in np.arange(nb_stateux)]
+
+x = np.array([[pm.Normal("x%d_%d" % (k,i), mu=v_mus[i], tau=v_taus[k], 
+                         observed=True, value=np.array(sample[k,i])) for i in np.arange(sample_length)]
+              for k in np.arange(nb_stateux)])
+
+mcmc = pm.MCMC([v_mus, v_sigmas, v_taus, x])
+mcmc.sample(5000,1000)
+
+mu_traces = [np.array(mcmc.trace('mu%d' % k)[:]) for k in np.arange(sample_length)]
+sigma_traces = [np.array(mcmc.trace("sigma%d" % i)[:]) for i in np.arange(nb_stateux)]
