70e106503124baad70729f02f3da350a42d7ad5c
yama1968 <ymartel@gmail.com>
2015-07-23 19:56:09 +0200

    a big more on dclone

--- rjags/7scientists_dclone.R
+++ rjags/7scientists_dclone.R
@@ -1,22 +1,32 @@
 
 library('dclone')
+library('rjags')
 
 x <- c(-27.020,3.570,8.191,9.898,9.603,9.945,10.056)
 n <- length(x)
 
 m <- NA
+cluster <- NA
 
-cluster <- makeCluster(spec = rep("localhost", 4),
-                       type = "PSOCK")
+chains <- 8
+nodes <- 4
+
+cluster <- makeCluster(spec = nodes,
+                       type = "SOCK")
+
+# cluster <- makeCluster(spec = nodes,
+#                        type = "MPI")
 
 m <- jags.parfit(cl = cluster,
                  model = '7scientists2.bug',
                  data = list('x' = x,
                              'n' = n),
-                 n.chains = 4,
+                 n.chains = chains,
                  n.adapt = 1000,
                  params = c("mu", "sigma"),
                  n.iter = 100000)
 
-
 summary(m)
+
+stopCluster(cluster)
+
