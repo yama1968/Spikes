bd5c54fbfe297858b7758f4bb314545ab187cd3d
yama1968 <ymartel@gmail.com>
2015-08-13 19:24:31 +0200

    some more vertica on avazu

--- vertica/vertica.avazu.R
+++ vertica/vertica.avazu.R
@@ -30,10 +30,10 @@ system.time({
 v.train <- tbl(v, "train")
 
 system.time({
-  collect(v.train %>%
-            select(click) %>%
-            group_by(click) %>%
-            summarise(count=n()))
+  c <- collect(v.train %>%
+                 select(click) %>%
+                 group_by(click) %>%
+                 summarise(count=n()))
 })
 # utilisateur     système      écoulé 
 # 0.180       0.000       3.898 
@@ -92,3 +92,64 @@ system.time({
 plot(log(per.device_model$nb[1:50]), type='l')
 
 db_save_view(v.per.device_model, "per_device_model")
+
+# mutation
+
+system.time ({
+  v.hod <- v.train %>%
+    mutate(hod=substr(hour,7,2), day=substr(hour, 5,2))
+})
+v.hod %>% select(id, hod, day)
+
+system.time({
+  v.per.hour <- v.hod %>%
+    group_by(hod) %>%
+    summarise(nb=n(), p=mean(click)) %>%
+    arrange(hod)
+  per.hour <- collect(v.per.hour)
+})
+
+system.time({
+  v.per.day.hour <- v.hod %>%
+    group_by(day,hod) %>%
+    summarise(nb=n(), p=mean(click)) %>%
+    arrange(day,hod)
+  per.day.hour <- collect(v.per.day.hour)
+})
+
+
+require(ggplot2)
+
+a <- ggplot(per.hour, aes(hwy)) +
+  geom_line(aes(x=hod, y=nb)) +
+  geom_line(aes(x=hod, y=p))
+a
+
+q <- sqlQuery(v$con@conn, "select hour, count(*) from train group by hour order by hour")
+q
+
+a <- ggplot(per.day.hour, aes(x=hod, y=nb)) +
+  geom_point(aes(color=day)) +
+  geom_smooth()
+a
+
+a <- ggplot(per.day.hour, aes(x=factor(hod), y=nb)) +
+  geom_boxplot()
+a
+
+a <- ggplot(per.day.hour, aes(x=factor(hod), y=p)) +
+  geom_boxplot()
+a
+
+a <- ggplot(per.day.hour, aes(x=hod, y=p)) +
+  geom_point(aes(color=day)) +
+  geom_smooth()
+a
+
+a <- ggplot(per.day.hour, aes(factor(day), p)) +
+  geom_boxplot()
+a
+
+
+# v.train %>% mutate(x=substr(hour,3,2)) %>% group_by(x) %>% summarise(nb=n())
+
