67da8e8e8d00828b440f5948b58dd553d3eff526
yama1968 <ymartel@gmail.com>
2015-10-26 22:07:02 +0100

    zero inflated gamma for biz debugged

--- rjags/biz/biz.zero.inflated.gamma.R2jags.R
+++ rjags/biz/biz.zero.inflated.gamma.R2jags.R
@@ -5,7 +5,7 @@ library('R2jags')
 # prepare data
 
 # biz <- c(30, 0, 0, 50, 0, 20, 0, 0, 120, 0, 0, 0, 80, 0)
-hbiz <- c(rep(0,11),
+data <- c(rep(0,11),
          14,
          0, 0,
          34,
@@ -33,39 +33,40 @@ chains <- 4
 nodes <- 4
 
 
-runbiz <- function (biz, nd = nodes, ch = chains) {
+runbiz <- function(b, nd = nodes, ch = chains) {
 
-    system.time (m <- jags.parallel(
-        model.file = 'biz.zero.inflated.gamma.bug',
-        data = list('biz' = biz),
-        n.chains = ch,
-        n.cluster = nd,
-        n.burnin = 1000,
-        inits = function()
-            list(group = 1*(biz > 0)),
+    system.time(m <- jags.parallel(
+        model.file    = 'biz.zero.inflated.gamma.bug',
+        data          = list('biz' = b),
+        n.chains      = ch,
+        n.cluster     = nd,
+        n.burnin      = 1000,
+        inits         = function()
+                          list(group = 1*(b > 0)),
         parameters.to.save = c('p', 'true.mean.rep', 'sigma', 'a', 'b', 'pval',
                                'Deviance', 'deviance', 'pD'),
-        jags.module = c("dic"),
-        n.iter = 6000,
-        DIC = TRUE
+        jags.module   = c("dic"),
+        n.iter        = 20000,
+        DIC           = TRUE,
+        n.thin        = 4
     ))
     
-
     m
 }
 
 
-runmul <- function (b = biz, steps = 3) {
+runmul <- function(b = data, steps = 3) {
     
-    N = length(b)
+    N       <- length(b)
+    shift   <- N %% (steps + 1)
     
     for (i in 1:(steps)) {
-        deb <- round(N*(i-1)/(steps+1)+1)
-        fin <- round(N*(i+1)/(steps+1))
+        deb <- round(N*(i - 1)/(steps + 1) + 1 + shift)
+        fin <- round(N*(i + 1)/(steps + 1) + shift)
         m <- runbiz(b[deb:fin])
         print(m)
     }
 }
 
-system.time(m <- runbiz(hbiz) )
+system.time(m <- runbiz(data) )
 m
