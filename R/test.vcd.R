fb02191d5893d4ae8ea04ff5e5287b6ab1c25d55
yama1968 <ymartel@gmail.com>
2015-11-02 20:42:14 +0100

    vcd

---
+++ R/test.vcd.R
@@ -0,0 +1,33 @@
+
+library(vcd)
+
+data("HairEyeColor")
+
+HEC <- structable(Eye ~ Sex + Hair, data = HairEyeColor)
+HEC
+
+mosaic(HEC)
+
+doubledecker(HEC)
+assoc(HEC)
+cotabplot(~Eye + Sex | Hair, data = HEC)
+cotabplot(~Eye + Hair | Sex, data = HEC)
+
+
+##
+
+STD <- structable(~Sex + Class + Age, data = Titanic[, , 2:1, ])
+cotabplot(~Class + Age | Sex, data = STD, split_verical = T)
+
+# 
+data("Arthritis")
+
+foo <- Arthritis
+foo$old <- foo$Age > 40
+foo$Age <- NULL
+Art <- structable(Improved ~ Treatment + Sex + old, 
+                  data = foo)
+cotabplot(Improved ~Treatment + old | Sex, data = Art)
+cotabplot(Improved ~Treatment + old | Sex, data = foo)
+
+
