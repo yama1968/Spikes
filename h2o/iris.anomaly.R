05917c618bb75df86b145482c57b1e8e2db507fc
yama1968 <ymartel@gmail.com>
2015-12-08 22:26:26 +0100

    mxnet & h2o

--- h2o/iris.anomaly.R
+++ h2o/iris.anomaly.R
@@ -10,9 +10,9 @@ iris.dl <- h2o.deeplearning(x=1:3,
                             training_frame=iris.h2o,
                             autoencoder=T,
                             activation="Tanh",
-                            l2=0.001,
-                            hidden=c(10, 2, 10),
-                            epochs=1)
+                            l2=0.0001,
+                            hidden=c(20, 10, 2, 10, 20),
+                            epochs=200)
 print(iris.dl)
 iris.anon <- as.data.frame(h2o.anomaly(iris.dl, iris.h2o))
 
@@ -21,3 +21,16 @@ iris$rmse <- iris.anon$Reconstruction.MSE
 hist(iris$rmse)
 
 head(iris,10)
+
+######
+
+features <- as.data.frame(h2o.deepfeatures(iris.dl, iris.h2o, layer = 3))
+features$label <- iris$Species
+
+library(ggplot2)
+
+qplot(DF.L3.C1, DF.L3.C2,
+      data    = features,
+      color   = label,
+      main    = "C1 vs C2")
+
