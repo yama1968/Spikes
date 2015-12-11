775720103f6b9d73149378aa2b1f354ffca0fe50
yama1968 <ymartel@gmail.com>
2015-12-11 18:38:58 +0100

    small changes

--- R/d.structs.R
+++ R/d.structs.R
@@ -1,18 +1,25 @@
 
 
 f <- function(x) 1:x
-g <- function(l) list(me = mean(l), mx = max(l))
-get.me <- function(l) lapply(l, function(x) x$me)
-get.mx <- function(l) lapply(l, function(x) x$mx)
+g <- function(l) list(me = mean(l), mx = max(l), ss2 = mean(l**2))
+get.me <- function(l) sapply(l, function(x) x$me)
+get.mx <- function(l) sapply(l, function(x) x$mx)
+get.ss2 <- function(l) sapply(l, function(x) x$ss2)
 
 library(dplyr)
 
-df2 <- data.frame(i = 1:3) %>%
-  mutate(x = lapply(i, f)) %>%
-  mutate(y = lapply(x, g)) %>%
-  mutate(z.me = get.me(y),
-         z.mx = get.mx(y)) %>%
-  select(i, z.me, z.mx)
+N = 10
 
-df2
+
+system.time({
+  df2 <- data.frame(i = 1:N) %>%
+    mutate(x = lapply(i, f)) %>%
+    mutate(y = lapply(x, g)) %>%
+    mutate(z.me  = get.me(y),
+           z.mx  = get.mx(y),
+           z.ss2 = (get.ss2(y))) %>%
+    select(-x, -y)
+  
+  print(mean(df2$z.ss2))
+})
 
