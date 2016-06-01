0bd88f561846ff7c304c5e0f01a56087c14d7d51
Yannick <ymartel@gmail.com>
2016-06-01 22:38:18 +0200

    playing with ML

--- R/arthritis.R
+++ R/arthritis.R
@@ -27,19 +27,21 @@ rf <- train(Improved~.-ID,
             tuneLength = 10)
 print(rf)
 
-
-linear <- train(Improved~.-ID,
-            data = Arthritis,
-            trControl = trControl,
-            method = "glmnet",
-            tuneLength = 20)
-print (linear)
-
 linear <- train(Improved~.-ID,
                 data = Arthritis,
                 trControl = trainControl("cv", 7, classProbs=TRUE),
                 method = "glmnet",
                 tuneLength = 20)
 print (linear)
+
+linear2 <- train(Improved~.-ID,
+                data          = Arthritis,
+                trControl     = trainControl("cv", 7, classProbs=TRUE),
+                preProcess    = c("BoxCox", "center", "scale"),
+                method        = "glmnet",
+                tuneLength    = 10)
+print (linear2)
+
+
 
 
--- R/logistic.regression.test01.R
+++ R/logistic.regression.test01.R
@@ -1,5 +1,8 @@
 # from http://mathewanalytics.com/2015/09/02/logistic-regression-in-r/
 
+library(doMC)
+registerDoMC(cores = 4)
+
 library(caret)
 data(GermanCredit)
 
@@ -10,7 +13,7 @@ training <- GermanCredit[Train,]
 testing <- GermanCredit[-Train,]
 
 mod_fit_one <- glm(Class ~ Age + ForeignWorker + Property.RealEstate +
-                           Housing.Own + CreditHistory.Critical,
+                     Housing.Own + CreditHistory.Critical,
                    data = training,
                    family = "binomial")
 
@@ -19,15 +22,26 @@ exp(coef(mod_fit_one))
 
 library(ROCR)
 
-get.auc <- function(model) {
+get.auc <- function(model, type="response") {
+  
+  prob <- predict(model, newdata=testing, type=type)
+  pred <- prediction(prob, testing$Class)
+  perf <- performance(pred, measure="tpr", x.measure="fpr")
+  plot(perf)
+  auc <- performance(pred, measure="auc")
+  auc <- auc@y.values[[1]]
+  auc
+}
 
-        prob <- predict(model, newdata=testing, type="response")
-        pred <- prediction(prob, testing$Class)
-        perf <- performance(pred, measure="tpr", x.measure="fpr")
-        plot(perf)
-        auc <- performance(pred, measure="auc")
-        auc <- auc@y.values[[1]]
-        auc
+get.auc2 <- function(model, type="prob") {
+  
+  prob <- predict(model, newdata=testing, type=type)
+  pred <- prediction(prob$Good, testing$Class)
+  perf <- performance(pred, measure="tpr", x.measure="fpr")
+  plot(perf)
+  auc <- performance(pred, measure="auc")
+  auc <- auc@y.values[[1]]
+  auc
 }
 
 get.auc(mod_fit_one)
@@ -36,11 +50,11 @@ mod_fit_two <- glm(Class ~ ., data=training, family="binomial")
 get.auc(mod_fit_two)
 
 three <- glm(Class ~ Amount + InstallmentRatePercentage + Telephone + ForeignWorker +
-                     CheckingAccountStatus.lt.0 + CheckingAccountStatus.0.to.200 +
-                     CheckingAccountStatus.gt.200 +
-                     CreditHistory.NoCredit.AllPaid + CreditHistory.ThisBank.AllPaid +
-                     CreditHistory.PaidDuly + SavingsAccountBonds.lt.100 +
-                     OtherInstallmentPlans.Bank,
+               CheckingAccountStatus.lt.0 + CheckingAccountStatus.0.to.200 +
+               CheckingAccountStatus.gt.200 +
+               CreditHistory.NoCredit.AllPaid + CreditHistory.ThisBank.AllPaid +
+               CreditHistory.PaidDuly + SavingsAccountBonds.lt.100 +
+               OtherInstallmentPlans.Bank,
              data=training,
              family="binomial")
 summary(three)
@@ -51,4 +65,41 @@ get.auc(three)
 #             family="binomial")
 # summary(four)
 # get.auc(four)
+
+
+g1 <- train(Class ~ . - Purpose.Vacation - Personal.Female.Single,
+            data          = training,
+            trControl     = trainControl("cv", 7, classProbs=TRUE, summaryFunction = twoClassSummary),
+            preProc       = c("BoxCox", "center", "scale"),
+            method        = "glmnet",
+            metric        = "ROC",
+            tuneLength    = 20)
+
+get.auc2(g1, type="prob")
+
+g2 <- train(Class ~ . - Purpose.Vacation - Personal.Female.Single,
+            data          = training,
+            trControl     = trainControl("adaptive_cv", 7, classProbs=TRUE, summaryFunction = twoClassSummary,
+                                         adaptive = list(min = 5,
+                                                         alpha = 0.05,
+                                                         method = "gls",
+                                                         complete = TRUE)),
+            preProc       = c("BoxCox", "center", "scale"),
+            method        = "glmnet",
+            metric        = "ROC",
+            tuneLength    = 20)
+get.auc2(g2, type="prob")
+
+cart1 <- train(Class ~ . - Purpose.Vacation - Personal.Female.Single,
+               data          = training,
+               trControl     = trainControl("adaptive_cv", 7, classProbs=TRUE, summaryFunction = twoClassSummary,
+                                            adaptive = list(min = 5,
+                                                            alpha = 0.05,
+                                                            method = "gls",
+                                                            complete = TRUE)),
+               method        = "rpart",
+               metric        = "ROC",
+               tuneLength    = 20)
+get.auc2(cart1, type="prob")
+
 
