e391aa03aa9948bb9ecdca14a7d78dc25b344394
Yannick MARTEL <yannick.martel@bleckwen.ai>
2019-10-07 09:34:04 +0200

    a few stuff

---
+++ Tensorflow-2/creditfraud_keras_sk_01.py
@@ -0,0 +1,120 @@
+#%%
+# from __future__ import absolute_import, division, print_function, unicode_literals
+
+"""
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+"""
+
+import tensorflow as tf
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+
+import numpy as np
+import pandas as pd
+import random
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import StratifiedKFold
+
+from evolutionary_search import EvolutionaryAlgorithmSearchCV
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+
+
+only_test = False
+
+data_file = "~/.kaggle/creditcard.csv"
+
+raw = pd.read_csv(data_file)
+if only_test: raw = raw.head(60000)
+raw.describe().T
+
+X = np.array(raw[["Amount", "Time"] + ["V%d" % i for i in range(1, 29)]])
+y = np.array(raw.Class)
+
+# X = np.array(raw[["Amount", "Time"] + ["V%d" % i for i in range(1, 29)]]).astype(np.float32)
+# y = np.array(raw.Class.astype(np.int64))
+
+
+scaler = StandardScaler()
+
+# X = scaler.fit_transform(X).astype(np.float32)
+X = scaler.fit_transform(X)
+
+print("loaded %s and got %s from %s" % (data_file, str((X.shape, y.shape)), raw.shape))
+raw = None
+
+(x_train, x_test, y_train, y_test) = train_test_split(X, y, 
+                                                      test_size=.33, 
+                                                      random_state=42,
+                                                      stratify=y)
+
+
+flatten = lambda l: [item for sublist in l for item in sublist]
+
+from sklearn.metrics import roc_auc_score, average_precision_score
+
+def aucpr(y_true, y_pred):
+    return tf.py_function(average_precision_score, (y_true, y_pred), tf.double)
+#%%
+def build_model(n_first=512, n_second=256, 
+                dropout_first=0., dropout_second=0.4,
+                add_layers=2,
+                learning_rate=0.001):
+  
+  layers = ([tf.keras.layers.Dense(n_first, activation='relu')] +
+            ([tf.keras.layers.Dropout(dropout_first)] if dropout_first > 0 else []) +
+            flatten([[tf.keras.layers.Dense(n_second / 2**i, activation='relu'),
+                     tf.keras.layers.Dropout(dropout_second)] for i in range(add_layers)]) +
+            [tf.keras.layers.Dense(1, activation='sigmoid')])
+  
+  model = tf.keras.models.Sequential(layers)
+  model.compile(optimizer='adam',
+                learning_rate=learning_rate,
+                loss='binary_crossentropy',
+                metrics=['AUC'])
+  return model
+#%%
+model = KerasClassifier(build_fn=build_model,
+                        add_layers=2,
+                        epochs=20,
+                        validation_split=0,
+                        batch_size=2048
+                        )
+
+history = model.fit(x_train, y_train)
+
+y_hat = model.predict_proba(x_test)[:,1]
+print("test score = %.3f" % average_precision_score(y_test, y_hat))
+#%%
+params = {
+    'epochs': [10, 20],
+    'n_first': 256 * 2**np.arange(3),
+    'n_second': 128 * 2**np.arange(3),
+    'dropout_first': 0.1 * np.arange(3, 6),
+    'dropout_second': 0.1 * np.arange(3, 6),
+    'add_layers': np.arange(2,4),
+    'batch_size': [2048, 4096],
+    'learning_rate': 1E-6 * 10 ** (np.arange(4)),
+}
+#%%
+random.seed(2)
+
+clf = KerasClassifier(build_fn=build_model, verbose=0, validation_split=0.)
+
+cv = EvolutionaryAlgorithmSearchCV(estimator            = clf,
+                                   params               = params,
+                                   scoring              = 'average_precision',
+                                   cv                   = StratifiedKFold(n_splits=3),
+                                   verbose              = 1,
+                                   population_size      = 20,
+                                   gene_mutation_prob   = 0.25,
+                                   gene_crossover_prob  = 0.5,
+                                   tournament_size      = 4,
+                                   generations_number   = 6,
+                                   n_jobs               = 1,
+                                   refit                = False)
+
+cv.fit(X, y)
+#%%
